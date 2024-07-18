a48473d3fa040a1fcf884065ad1f46576947f477
Maigaona <espinosagaonamaya@gmail.com>
2024-07-18 17:15:48 -0600

    inicio terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { AcercadeComponent } from './acercade/acercade.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, AcercadeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
